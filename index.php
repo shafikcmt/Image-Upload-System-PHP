730ac501d35e9d1c63344e834f4f2f255ca03ced
shafikcmt <mdshafiqulislam822@gmail.com>
2021-08-14 03:32:26 +0600

    Upload image in database and folder php mysqli

--- index.php
+++ index.php
@@ -7,7 +7,23 @@ $db = new Database();
       
         <div class="panel-body">
             <div style="width:600px; margin:0 auto;">
-        
+            <?php
+                 if($_SERVER['REQUEST_METHOD']== "POST") {
+                     $permited = array('jpg','jpeg','png','gif');
+                     $file_name = $_FILES['image']['name'];
+                     $file_size = $_FILES['image']['size'];
+                     $file_temp = $_FILES['image']['tmp_name'];
+                     $folder = "uploads/";
+                     move_uploaded_file($file_temp,$folder.$file_name);
+                     $query = "INSERT INTO tbl_image(image )VALUES('$file_name')";
+                     $insert_rows = $db->insert($query);
+                     if($insert_rows){
+                         echo "<div class='alert alert-success'>Image Inserted Successfully</div>";
+                     }else{
+                         echo "<div class='alert alert-danger'>Image not Inserted !</div>";
+                     }
+                    } 
+                     ?>
                 <form action="" method="post" enctype="multipart/form-data">
                     <div class="form-group">
                         <label for="img">Select Image</label>
--- lib/Database.php
+++ lib/Database.php
@@ -36,6 +36,6 @@ class Database{
             return false;
         }
     }
-   
+  
 }
 ?>
